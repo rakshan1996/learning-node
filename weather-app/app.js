0777fd1309558e811465b08266cb5ac11d87ed4d
Rakshan Thapar <rakshanthapar32@gmail.com>
2018-11-27 22:35:34 +0530

    added try catch Block on error is we are unable to connect to server

--- weather-app/app.js
+++ weather-app/app.js
@@ -19,13 +19,19 @@ request({
     url:`http://www.mapquestapi.com/geocoding/v1/address?key=yLFBAtZxOtgcZSgAw2Ae1SSAXPG1hQZD&location=${encodedAddress}`,
     json: true
 },(error,response,body)=>{
-    /* console.log(error);
-    console.log(response); */
- console.log(JSON.stringify(body,undefined,4));
-
- console.log(`Address enterd: ${body.results[0].providedLocation.location}`)
- latitude=body.results[0].locations[0].latLng.lat;
-longtitude=body.results[0].locations[0].latLng.lng;
-
-console.log(`lattitude:${latitude} => longtitude:${longtitude}`);
+    /* /* console.log(error);
+    console.log(response); 
+    console.log(JSON.stringify(body,undefined,4));
+    console.log(`Address enterd: ${body.results[0].providedLocation.location}`) */ 
+    if(error){
+        console.log("Unable to connect to a google server");
+    }
+    else 
+    {
+        latitude=body.results[0].locations[0].latLng.lat;
+        longtitude=body.results[0].locations[0].latLng.lng;
+        
+        console.log(`lattitude:${latitude} => longtitude:${longtitude}`);
+    }
+  
 });
