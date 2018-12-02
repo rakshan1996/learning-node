c0862ea6c63cce5ec1d789a3063e5b039574dc3b
Rakshan Thapar <rakshanthapar32@gmail.com>
2018-12-03 01:00:30 +0530

    Set up a static Directory that makes the code run

--- node-webServer/server.js
+++ node-webServer/server.js
@@ -1,8 +1,23 @@
 const express=require('express');
 
 var app=express();
+
+app.use(express.static(__dirname+'/public'));
+
 app.get('/',(req,res)=>{
-res.send('<h1>Hello World Devil this side</h1>');
+//res.send('<h1>Hello World Devil this side</h1>');
+res.send({
+    name:'Devil',
+    likes:[
+        'Food',
+        'Hiking'
+    ]
+})
+});
+app.get('/about',(req,res)=>{
+res.send("Hi i am Devil and i am 6 ft 2");
 });
 
-app.listen(3000);
+app.listen(3000,()=>{
+    console.log("server is running at port 3000");
+});
