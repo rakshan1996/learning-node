21869147feaca0279136b412e6002fadf34fd293
Rakshan Thapar <rakshanthapar32@gmail.com>
2018-12-04 01:29:20 +0530

    Created handlebars partial template and function and used them by registering themin server.js file with registerPartials and registerHelper

--- node-webServer/server.js
+++ node-webServer/server.js
@@ -2,6 +2,13 @@ const express=require('express');
 const hbs=require('hbs');
 
 var app=express();
+hbs.registerPartials(__dirname+'/views/partials');
+hbs.registerHelper('getCurrentYear',()=>{
+    return new Date().getFullYear();
+});
+hbs.registerHelper('ScreamIt',(text)=>{
+    return text.toUpperCase();
+});
 app.set('view engine','hbs');
 app.use(express.static(__dirname+'/public'));
 
@@ -19,13 +26,13 @@ res.render('home.hbs',{
     Title:"Devil's Pc",
     authorsName:"satan",
     welcomeMessage:"This is the Entry to Hell Gate Proceed with caution",
-    Copyright: new Date()
+    //Copyright: new Date()
 })
 });
 app.get('/about',(req,res)=>{
 res.render('about.hbs',{
-    pageTitle: 'About Page',
-    currentYear: new Date().getFullYear()
+    authorsName: 'About Page',
+    //Copyright: new Date().getFullYear()
 });
 });
 
