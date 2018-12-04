22b86628a39643cebd99145609577f9c878feecd
Rakshan Thapar <rakshanthapar32@gmail.com>
2018-12-05 00:56:38 +0530

    Added Version Control System and created account on Heroku to host the app configured server.js and Heroku cli

--- node-webServer/server.js
+++ node-webServer/server.js
@@ -1,6 +1,7 @@
 const express=require('express');
 const hbs=require('hbs');
 const fs=require('fs');
+const port=process.env.PORT ||3000;
 
 var app=express();
 hbs.registerPartials(__dirname+'/views/partials');
@@ -48,6 +49,6 @@ res.render('about.hbs',{
 });
 });
 
-app.listen(3000,()=>{
-    console.log("server is running at port 3000");
+app.listen(port,()=>{
+    console.log(`server is running at port ${port}`);
 });
