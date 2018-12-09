2e3f1ec1c838da02afa2864da7371a605c9f82b2
Rakshan Thapar <rakshanthapar32@gmail.com>
2018-12-09 16:27:33 +0530

    Learned about object destructuring and object ID in case of MongoDB

--- To-Do app/playground/mongoDbConnect.js
+++ To-Do app/playground/mongoDbConnect.js
@@ -1,4 +1,14 @@
-const MongoClient=require('mongodb').MongoClient;
+//const MongoClient=require('mongodb').MongoClient;
+
+const {MongoClient,ObjectID}=require('mongodb');
+
+//object destructring
+/* 
+var user={name:'andrew',age:25};
+
+var {name}=user;
+console.log(name); */
+
 
 MongoClient.connect('mongodb://localhost:27017/TodoApp',(err,client)=>{
     if(err){
@@ -18,7 +28,7 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp',(err,client)=>{
         console.log(JSON.stringify(result.ops,undefined,2));
     });
  */
-        db.collection('Users').insertOne({
+        /* db.collection('Users').insertOne({
             name:"Devil",
             age:22,
             location:"New Delhi"
@@ -26,8 +36,8 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp',(err,client)=>{
             if(err){
                 return console.log("unable to add collection",err);
             }
-            console.log(JSON.stringify(result.ops,undefined,2));
+            console.log(JSON.stringify(result.ops[0]._id.getTimestamp(),undefined,2));
         });
-
+         */
     client.close();
 });
